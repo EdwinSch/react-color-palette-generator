007bc496f98efdd84b3c086cfbb5a1fbbb4ff84f
front-ed <edwinschutjes@gmail.com>
2023-05-20 16:04:05 +0200

    add: color components and functionality

--- src/components/ColorList.jsx
+++ src/components/ColorList.jsx
@@ -1,5 +1,14 @@
-const ColorList = () => {
-  return;
+import SingleColor from "./SingleColor";
+import { nanoid } from "nanoid";
+
+const ColorList = ({ colors }) => {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />;
+      })}
+    </section>
+  );
 };
 
 export default ColorList;
--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -1,5 +1,15 @@
-const SingleColor = () => {
-  return;
+const SingleColor = ({ color, index }) => {
+  const { hex, weight } = color;
+
+  return (
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ background: `#${hex}` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+    </article>
+  );
 };
 
 export default SingleColor;
